02fcc86cfa37129c569e989e52f598474135078c
F baffour <f.baffour@outlook.com>
2023-12-09 14:19:35 +0100

    first commit

---
+++ src/main/java/nl/novi/easytech/EasyTechApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.easytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/easytech/EasyTechApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.easytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
